60ec6455b166839a163e9a5de0502ccbe5c8fbb0
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-03-02 18:12:18 -0300

    Add DB connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  'mongodb+srv://denies:WX1dkJotHMfz6hgn@main-database.t1g2w.mongodb.net/main-database?retryWrites=true&w=majority',
+  process.env.DB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
 );
 
